172687e7d12dda32e673cf40bd6301b95f6159a3
Adi <adrianjoshualitawan@gmail.com>
2024-12-07 22:41:32 +0800

    Update mainscript.js

--- css/js/mainscript.js
+++ css/js/mainscript.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    // Dynamically set the favicon
     const link = document.createElement('link');
     link.rel = 'icon';
     link.href = 'css/images/logonav.png';
@@ -8,10 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
     // Logout functionality
     const logoutLink = document.getElementById('logoutLink');
     if (logoutLink) {
-        logoutLink.addEventListener('click', function(event) {
-            event.preventDefault();
-            sessionStorage.removeItem('loggedIn');
-            window.location.href = "index.html";
+        console.log('Logout link found');
+        logoutLink.addEventListener('click', function (event) {
+            console.log('Logout clicked');
+            event.preventDefault(); // Prevent the default anchor action
+            sessionStorage.removeItem('loggedIn'); // Remove logged-in status
+            window.location.href = "index.html"; // Redirect to the index page
         });
     } else {
         console.error("Logout link not found");
