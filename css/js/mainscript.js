577382f69a446cfef0ab3cc9d47ecf825e6098a9
Adi <adrianjoshualitawan@gmail.com>
2024-12-07 22:39:20 +0800

    Update mainscript.js

--- css/js/mainscript.js
+++ css/js/mainscript.js
@@ -1,20 +1,26 @@
-function checkLoginStatus() {
+document.addEventListener('DOMContentLoaded', function() {
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = 'css/images/logonav.png';
+    link.type = 'image/png';
+    document.head.appendChild(link);
+
+    // Logout functionality
+    const logoutLink = document.getElementById('logoutLink');
+    if (logoutLink) {
+        logoutLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            sessionStorage.removeItem('loggedIn');
+            window.location.href = "index.html";
+        });
+    } else {
+        console.error("Logout link not found");
+    }
+
+    // Check login status
     const loggedIn = sessionStorage.getItem('loggedIn');
     if (loggedIn !== 'true') {
         alert("You are not logged in");
         window.location.href = "index.html";
     }
-}
-checkLoginStatus();
-
-const link = document.createElement('link');
-    link.rel = 'icon';
-    link.href = 'css/images/logonav.png';
-    link.type = 'image/png';
-    document.head.appendChild(link);"
-
-document.getElementById('logoutLink').addEventListener('click', function (event) {
-    event.preventDefault();
-    sessionStorage.removeItem('loggedIn');
-    window.location.href = "index.html";
 });
